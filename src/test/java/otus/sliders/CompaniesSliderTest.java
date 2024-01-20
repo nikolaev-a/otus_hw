9d80697b177fe680a49845c20d432e0567160ecf
Artem Nikolaev <a.nikolaev@rts-tender.ru>
2024-01-20 21:26:17 +0300

    Реализовано - DI страницы

--- src/test/java/otus/sliders/CompaniesSliderTest.java
+++ src/test/java/otus/sliders/CompaniesSliderTest.java
@@ -1,31 +1,29 @@
 package otus.sliders;
 
-import anotations.Driver;
-
 import anotations.Page;
 
 import extensions.UIExtensions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.WebDriver;
 import pages.MainPage;
 
 @ExtendWith(UIExtensions.class)
 public class CompaniesSliderTest {
 
-    @Driver
-    WebDriver driver;
+//    @Driver
+//    WebDriver driver;
 
     @Page
-    private MainPage mainPage;
+    MainPage mainPage;
 
     @Test
     public void testTest() throws InterruptedException {
 
-        new MainPage(driver).open()
+        mainPage.open()
                 .checkPageOpeningMarker()
                 .highlightElement()
                 .removeHighlightElement()
                 .filterByCourseName("Специализация сетевой инженер");
+
     }
 }
